68d99ee0132edb7740e015a9fd66147a26ff77d8
kuibobo <kuibobo@gmail.com>
2013-12-27 14:53:21 +0800

    test

--- app/index.php
+++ app/index.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9" />
 <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
-<title>JiaMi edit by kuibobo</title>
+<title>JiaMi edit by kuibobo2</title>
 <link rel="stylesheet" href="../theme/css/global.css" type="text/css">
 <link rel="stylesheet" href="../theme/css/order.css" type="text/css">
 </head>
